1b8cc557d4559ae4bec97a44940f8004c8ea821f
ByFxbian <65723620+ByFxbian@users.noreply.github.com>
2025-10-17 13:43:28 +0200

    insta entfernt

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -46,15 +46,6 @@ export default function BookingForm({ barbers, services, hasFreeAppointment  }: 
       return;
     }
 
-    /*if (!session.user.emailVerified) {
-      alert('Bitte bestätige zuerst deine E-Mail-Adresse, bevor du einen Termin buchst.');
-      return;
-    }*/
-   if(!session.user.instagram) {
-    alert('Bitte füge in den Einstellungen zuerst dein Instagram hinzu, bevor du einen Termin buchst.');
-    return
-   }
-
     setIsBooking(true);
 
     const res = await fetch('/api/appointments', {
